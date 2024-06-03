0ccdfe9f5ae3414cbd3da335b221156f0c403a3f
Murtazakatyar <168894683+Murtazakatyar@users.noreply.github.com>
2024-06-03 16:24:47 +0500

    Add files via upload
    dart operators
    Arithmetic operator
    comparision operator
    Assigment operator

---
+++ opretors.dart
@@ -0,0 +1,37 @@
+
+
+void main() {
+  //arithmetic operator
+  // var a = 10;
+  // var b = 20;
+
+  // print(a + b);
+  // print(a - b);
+  // print(a * b);
+  // print(a / b);
+  // print(a % b);
+
+// //relational operators
+
+//   var a = 10;
+//   var b = 20;
+
+//   print(a ==b);
+//   print(a > b);
+//   print(a < b);
+//   print(a <= b);
+//   print(a >= b);
+//   print(a != b);
+
+//logical operators
+
+  // var a = 20;
+  // var b = 20;
+
+  // if (a == b && b == a) {
+  //   print(' Conditioon is true');
+  // } else {
+  //   print('condition false');
+  // }
+
+}
